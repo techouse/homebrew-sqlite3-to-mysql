b1c42d0692177c6f56afcdcabf173a86478fe8d7
Klemen Tušar <techouse@gmail.com>
2023-12-16 14:49:45 +0000

    :tada: initial commit

---
+++ Formula/sqlite3-to-mysql.rb
@@ -0,0 +1,72 @@
+class Sqlite3ToMysql < Formula
+  include Language::Python::Virtualenv
+
+  desc "Transfer data from SQLite to MySQL"
+  homepage "https://github.com/techouse/sqlite3-to-mysql"
+  url "https://files.pythonhosted.org/packages/06/46/ce49ef8c247069e3fe2b67c4aab1db45eb9dd5051dd53ade1c23e1c33f25/sqlite3_to_mysql-2.1.6.tar.gz"
+  sha256 "99e9a9caddf9cb18e7cabe19810c1dc1196f7b2acb6bf51a6a4de5529496902b"
+  license "MIT"
+
+  depends_on "python3"
+  depends_on "sqlite"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
+  resource "mysql-connector-python" do
+    url "https://github.com/mysql/mysql-connector-python/archive/refs/tags/8.2.0.tar.gz"
+    sha256 "29a7cb0b24a4f5ccda597c211254db8377493e95e36ca4ee05b15dba6e9c6e78"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/fb/2b/9b9c33ffed44ee921d0967086d653047286054117d584f1b1a7c22ceaf7b/packaging-23.2.tar.gz"
+    sha256 "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
+  end
+
+  resource "protobuf" do
+    url "https://files.pythonhosted.org/packages/ba/dd/f8a01b146bf45ac12a829bbc599e6590aa6a6849ace7d28c42d77041d6ab/protobuf-4.21.12.tar.gz"
+    sha256 "7cd532c4566d0e6feafecc1059d04c7915aec8e182d1cf7adee8b24ef1e2e6ab"
+  end
+
+  resource "pytimeparse2" do
+    url "https://files.pythonhosted.org/packages/19/10/cc63fecd69905eb4d300fe71bd580e4a631483e9f53fdcb8c0ad345ce832/pytimeparse2-1.7.1.tar.gz"
+    sha256 "98668cdcba4890e1789e432e8ea0059ccf72402f13f5d52be15bdfaeb3a8b253"
+  end
+
+  resource "simplejson" do
+    url "https://files.pythonhosted.org/packages/79/79/3ccb95bb4154952532f280f7a41979fbfb0fbbaee4d609810ecb01650afa/simplejson-3.19.2.tar.gz"
+    sha256 "9eb442a2442ce417801c912df68e1f6ccfcd41577ae7274953ab3ad24ef7d82c"
+  end
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/ec/fe/802052aecb21e3797b8f7902564ab6ea0d60ff8ca23952079064155d1ae1/tabulate-0.9.0.tar.gz"
+    sha256 "0095b12bf5966de529c0feb1fa08671671b3368eec77d7ef7ab114be2c068b3c"
+  end
+
+  resource "tqdm" do
+    url "https://files.pythonhosted.org/packages/62/06/d5604a70d160f6a6ca5fd2ba25597c24abd5c5ca5f437263d177ac242308/tqdm-4.66.1.tar.gz"
+    sha256 "d88e651f9db8d8551a62556d3cff9e3034274ca5d66e93197cf2490e2dcb69c7"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/0c/1d/eb26f5e75100d531d7399ae800814b069bc2ed2a7410834d57374d010d96/typing_extensions-4.9.0.tar.gz"
+    sha256 "23478f88c37f27d76ac8aee6c905017a143b0b1b886c3c9f66bc2fd94f9f5783"
+  end
+
+  resource "Unidecode" do
+    url "https://files.pythonhosted.org/packages/80/5d/f156f6a7254ecc0549de0eb75f786d2df724c0310b97c825383517d2c98d/Unidecode-1.3.7.tar.gz"
+    sha256 "3c90b4662aa0de0cb591884b934ead8d2225f1800d8da675a7750cbc3bd94610"
+  end
+
+  def install
+    virtualenv_create(libexec, "python3")
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "Usage: sqlite3mysql [OPTIONS]", shell_output("#{bin}/sqlite3mysql --help")
+    assert_match "/sqlite3\-to\-mysql\s+\|\s2.1.6/", shell_output("#{bin}/sqlite3mysql --version")
+  end
+end
